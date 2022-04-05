e115543070ca4f83ac1d07fc66d848e468011ade
Lukas Braun <lukas.braun@handtmann.de>
2022-04-05 09:45:59 +0200

    LB: added reading of timestamps, not tested

--- timediff.asm
+++ timediff.asm
@@ -17,6 +17,23 @@
 
 %include "syscall.inc"  ; OS-specific system call macros
 
+sys_read equ 2
+sys_write equ 4
+stdout equ 1
+stdin equ 2
+
+;-----------------------------------------------------------------------------
+; SECTION DATA
+;-----------------------------------------------------------------------------
+SECTION .data
+        userMsg db 'Please enter a timestamp: ' ;Message to ask the User to Enter a new timestamp
+        lenUserMsg equ $-userMsg                ;The length of the message
+
+;-----------------------------------------------------------------------------
+; SECTION BSS
+;-----------------------------------------------------------------------------
+SECTION .bss
+        timestamp_input resb 100
 
 ;-----------------------------------------------------------------------------
 ; SECTION TEXT
@@ -26,9 +43,26 @@ SECTION .text
         ;-----------------------------------------------------------
         ; PROGRAM'S START ENTRY
         ;-----------------------------------------------------------
-        global _start:function  ; make label available to linker
-_start:
-        nop
+        global _start:function          ; make label available to linker
+
+_start:                                 ; Programm Start
+        
+        ; <<<<<<<--TODO Test-->>>>>>>
+        ; Write the initial User Message
+        mov eax, sys_write              ; Sys-Call Number (Write)
+        mov ebx, stdout                 ; file discriptor (STD OUT)
+        mov ecx, userMsg                ; length of the Message
+        mov edx, lenUserMsg             ; Message to write
+        int 80h                         ; call Kernel
+
+        ;Read and store the user input
+        mov eax, sys_read               ; Sys-Call Number (Read)
+        mov ebx, stdin                  ; file discriptor (STD IN)
+        mov ecx, timestamp_input        ; input ist stored into num
+        mov edx, 8                      ; size of Input
+        int 80h                         ; call Kernel
+        
+        ; <<<<<<<---->>>>>>>
 
         ;-----------------------------------------------------------
         ; call system exit and return to operating system / shell
