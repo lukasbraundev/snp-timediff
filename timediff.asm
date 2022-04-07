17ebc5e4dfe734f41aff055a4a35d1da05be68cf
Lukas Braun <lb.0108@web.de>
2022-04-07 18:32:28 +0200

    LB: Timestamp input

--- timediff.asm
+++ timediff.asm
@@ -17,7 +17,7 @@
 
 %include "syscall.inc"  ; OS-specific system call macros
 
-sys_read equ 2
+sys_read equ 3
 sys_write equ 4
 stdout equ 1
 stdin equ 2
@@ -26,14 +26,14 @@ stdin equ 2
 ; SECTION DATA
 ;-----------------------------------------------------------------------------
 SECTION .data
-        userMsg db 'Please enter a timestamp: ' ;Message to ask the User to Enter a new timestamp
+        userMsg db 'Please enter a timestamp (to end write "F"): ' ;Message to ask the User to Enter a new timestamp
         lenUserMsg equ $-userMsg                ;The length of the message
 
 ;-----------------------------------------------------------------------------
 ; SECTION BSS
 ;-----------------------------------------------------------------------------
 SECTION .bss
-        timestamp_input resb 100
+        timestamp_input resb 101280
 
 ;-----------------------------------------------------------------------------
 ; SECTION TEXT
@@ -46,8 +46,9 @@ SECTION .text
         global _start:function          ; make label available to linker
 
 _start:                                 ; Programm Start
+
+readNextTimestamp:
         
-        ; <<<<<<<--TODO Test-->>>>>>>
         ; Write the initial User Message
         mov eax, sys_write              ; Sys-Call Number (Write)
         mov ebx, stdout                 ; file discriptor (STD OUT)
@@ -58,11 +59,25 @@ _start:                                 ; Programm Start
         ;Read and store the user input
         mov eax, sys_read               ; Sys-Call Number (Read)
         mov ebx, stdin                  ; file discriptor (STD IN)
-        mov ecx, timestamp_input        ; input ist stored into num
-        mov edx, 8                      ; size of Input
+        mov ecx, timestamp_input        ; input ist stored into timestamp_input
+        mov edx, 128                    ; size of Input
         int 80h                         ; call Kernel
-        
-        ; <<<<<<<---->>>>>>>
+
+        ;Compare if timestamp is F
+        lea     rsi, [timestamp_input]  ; load the address of buffer to rsi
+        movzx   rdx, byte [rsi]         ; load first char to
+        cmp     rdx, 70                 ; check if first char is "F"
+        je      finishedInput           ; if its F input is finished
+        ;TODO --> Add to list
+        jne     readNextTimestamp       ; not F input not finished
+
+finishedInput:
+        ;Test Purpose to test the Jump
+        mov eax, sys_write              ; Sys-Call Number (Write)
+        mov ebx, stdout                 ; file discriptor (STD OUT)
+        mov ecx, userMsg                ; length of the Message
+        mov edx, lenUserMsg             ; Message to write
+        int 80h                         ; call Kernel
 
         ;-----------------------------------------------------------
         ; call system exit and return to operating system / shell
