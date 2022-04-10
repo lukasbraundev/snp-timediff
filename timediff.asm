f5bd69c71de001107033852ce70d7dccb02ed940
Lukas Braun <lb.0108@web.de>
2022-04-10 16:17:53 +0200

    LB: WIP

--- timediff.asm
+++ timediff.asm
@@ -32,7 +32,7 @@ extern list_get
 sys_read equ 3
 sys_write equ 4
 stdout equ 1
-stdin equ 2
+stdin equ 0
 
 BUFFER_SIZE equ 80
 
@@ -49,15 +49,18 @@ timeval:
 
         timestamp db '97.98'
         lenTimestamp equ $-timestamp
-        time_char db '10000 00000000000000.000000'
+        time_char db '1000000000000000000.000000'
         lenTimeChar equ $-time_char
-
+        possible_timechar db '                            '
+        possible_timechar_len equ $-possible_timechar
 ;-----------------------------------------------------------------------------
 ; SECTION BSS
 ;-----------------------------------------------------------------------------
 SECTION .bss
         timestamp_input resb 128
-
+                align 128               ; make sure that buffer adress is a multiple of 128
+        buffer  resb BUFFER_SIZE        ; buffersize for input
+                resb 128                ; zero after buffer so that buffer end can be determined
 
 ;-----------------------------------------------------------------------------
 ; SECTION TEXT
@@ -100,31 +103,74 @@ _start:                                 ; Programm Start
 
 ; Beispiel END
 
-readNextTimestamp:
-        ;Read and store the user input
-        mov eax, sys_read               ; Sys-Call Number (Read)
-        mov ebx, stdin                  ; file discriptor (STD IN)
-        mov ecx, timestamp_input        ; input is stored into timestamp_input
-        mov edx, 128                    ; size of Input (Byte)
+init:
+        mov r12, 0                      ; ctr for the possible timechar index
+
+read_next_string:
+        ; Read from STD in
+        mov rax, sys_read               ; Sys-Call Number (Read)
+        mov rbx, stdin                  ; file discriptor (STD IN)
+        mov rcx, buffer                 ; input is stored into timestamp_input
+        mov rdx, BUFFER_SIZE            ; size of Input
         int 80h                         ; call Kernel
 
-        ;Compare if timestamp is F
-        lea     rsi, [timestamp_input]  ; load the address of buffer to rsi
-        movzx   rdx, byte [rsi]         ; load first char to
-        cmp     rdx, 70                 ; check if first char is "F"
-        je      finishedInput           ; if its F input is finished
-        ;TODO --> Add to list
-        jne     readNextTimestamp       ; not F input not finished
+        test    rax,rax                 ; check system call return value
+        jz      finishedInput             ; jump to exit if nothing is read (end)
+        lea rsi, [buffer]               ; loads adress of first char into rsi
+        mov byte [buffer+rax], 128      ; determines the End of the Buffer
 
+
+
+
+next_char:      
+        mov     dl, byte [rsi]          ; load next char from buffer
+        cmp     dl,127                  ; check if its a char
+        ja      read_next_string        ; jump if no char
+        cmp     r12, 27                 ; check if input length is max
+        je      max_input_error
+        cmp     dl, 10                  ; check for linefeed TODO, check for end of file
+        je      timestamp_finished      ; jump if timestamp detected
+        mov     [possible_timechar + r12], dl
+        inc r12                         ; inc possible timechar index
+        inc rsi                         ; inc adress in Buffer
+        jmp next_char
+
+
+max_input_error:
+
+        jmp exit
+
+timestamp_finished:
+        ; Check for correct syntax
+        push rsi
+        push rdx
+        mov rdi, timeval
+        mov rsi, possible_timechar
+        xor rdx, rdx
+        mov dx, possible_timechar_len
+        call ASCII_to_timeval
+        pop rdx
+        pop rsi
+        cmp rax, 0                      ; check if conversation was sucessful
+        je exit                         ; if not exit programnm
+        ; ----- TODO Error Message -------- ;
+        mov rdi, timeval
+        call list_add
+        ; addList() Funktion aufrufen TODO Rückgabewert überprüfen
+        ; placeholder reinitialize
+        ; foreach char in possible_char
+        ; char => " "
+        mov r12, 0
+        jmp next_char
 
 
 finishedInput:
         nop
 
-        ;-----------------------------------------------------------
-        ; call system exit and return to operating system / shell
-        ;-----------------------------------------------------------
-.exit:  SYSCALL_2 SYS_EXIT, 0
+;         ;-----------------------------------------------------------
+;         ; call system exit and return to operating system / shell
+;         ;-----------------------------------------------------------
+exit:  SYSCALL_2 SYS_EXIT, 0
         ;-----------------------------------------------------------
         ; END OF PROGRAM
         ;-----------------------------------------------------------
